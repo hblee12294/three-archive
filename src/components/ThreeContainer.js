729092809c541cd7a0ea4d500f30185733abc50c
Hongbin Li <hblee12294@gmail.com>
2018-11-11 13:12:13 -0800

    feat: added description marquee

--- src/components/ThreeContainer.js
+++ src/components/ThreeContainer.js
@@ -11,8 +11,16 @@ export default class ThreeContainer extends Component {
       <div className="container">
         <div
           className="three-root"
-          ref={ element => this.threeRootElement = element }
-        />
+          ref={element => (this.threeRootElement = element)}
+        >
+          <div className="info-icon">I</div>
+          <div className="marquee">
+            <div className="marquee-inner">
+              <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+              <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
